13f62a8be5b9f282053106262ba6363589130356
ihatecsv <me@drakeluce.com>
2017-06-30 16:24:54 -0300

    Getting started with the class, data queue

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var SerialPort = require("serialport");
 
-exports.exampleFunc = function(){
-	return "foobar";
+class GQGeiger {
+	constructor(port, baud){
+		this.geigerPort = new SerialPort(port, {baudrate: baud});
+		this.queue = [];
+		geigerPort.on('data', function(data) {
+			const cD = queue.shift();
+			cD.internalCallback(externalCallback, data);
+		});
+	}
+	
+	processCPM(callback, buffer){
+		cpm = buff.readUInt16BE(0);
+		callback(null, cpm);
+	}
+	getCPM(callback){
+		geigerPort.write(new Buffer("<GETCPM>>", 'ascii'), function(err, results) {});
+		queue.push({internalCallback: processCPM, externalCallback: callback});
+	}
 }
+
+module.exports = GQGeiger;
