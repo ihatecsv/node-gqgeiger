2941b3c8c4b823e5cbbeab72989f2f367655e780
ihatecsv <me@drakeluce.com>
2017-06-30 20:27:56 -0300

    Added errpr handling for buffer stuff

--- index.js
+++ index.js
@@ -27,57 +27,81 @@ class GQGeiger {
 	}
 	
 	processCPM(buffer, callback) {
-		const cpm = buffer.readUInt16BE(0);
-		callback(null, cpm);
+		try{
+			const cpm = buffer.readUInt16BE(0);
+			callback(null, cpm);
+		}catch(e){
+			callback(e, null);
+		}
 	}
 	getCPM(callback){
 		this.reqData("<GETCPM>>", this.processCPM, callback);
 	}
 	
 	processDoseRate(buffer, callback){
-		const doseRate = buffer.readUInt16BE(0)/200;
-		callback(null, doseRate);
+		try{
+			const doseRate = buffer.readUInt16BE(0)/200;
+			callback(null, doseRate);
+		}catch(e){
+			callback(e, null);
+		}
 	}
 	getDoseRate(callback){
 		this.reqData("<GETCPM>>", this.processDoseRate, callback);
 	}
 	
 	processVoltage(buffer, callback){
-		const voltage = buffer.readUInt8(0)/10;
-		callback(null, voltage);
+		try{
+			const voltage = buffer.readUInt8(0)/10;
+			callback(null, voltage);
+		}catch(e){
+			callback(e, null);
+		}
 	}
 	getVoltage(callback){
 		this.reqData("<GETVOLT>>", this.processVoltage, callback);
 	}
 	
 	processSerial(buffer, callback){
-		const serial = buffer.toString('hex');
-		callback(null, serial);
+		try{
+			const serial = buffer.toString('hex');
+			callback(null, serial);
+		}catch(e){
+			callback(e, null);
+		}
 	}
 	getSerial(callback){
 		this.reqData("<GETSERIAL>>", this.processSerial, callback);
 	}
 	
 	processTemp(buffer, callback){
-		const integerPart = buffer.readUInt8(0);
-		const decimalPart = buffer.readUInt8(1);
-		const negative = buffer.readUInt8(2);
-		let temp = parseFloat(integerPart + "." + decimalPart);
-		if(negative){
-			temp = 0 - temp;
+		try{
+			const integerPart = buffer.readUInt8(0);
+			const decimalPart = buffer.readUInt8(1);
+			const negative = buffer.readUInt8(2);
+			let temp = parseFloat(integerPart + "." + decimalPart);
+			if(negative){
+				temp = 0 - temp;
+			}
+			callback(null, temp);
+		}catch(e){
+			callback(e, null);
 		}
-		callback(null, temp);
 	}
 	getTemp(callback){
 		this.reqData("<GETTEMP>>", this.processTemp, callback);
 	}
 	
 	processGyro(buffer, callback){
-		let gyro = {};
-		gyro.x = buffer.slice(0, 2);
-		gyro.y = buffer.slice(2, 4);
-		gyro.z = buffer.slice(4, 6);
-		callback(null, gyro);
+		try{
+			let gyro = {};
+			gyro.x = buffer.slice(0, 2);
+			gyro.y = buffer.slice(2, 4);
+			gyro.z = buffer.slice(4, 6);
+			callback(null, gyro);
+		}catch(e){
+			callback(e, null);
+		}
 	}
 	getGyro(callback){
 		this.reqData("<GETGYRO>>", this.processGyro, callback);
